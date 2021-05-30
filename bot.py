975e3765d687f372b4e9bb8a24dc731e7e519a84
DuhonTheGuy <duhontheguy@gmail.com>
2021-05-30 12:12:56 -0400

    Changed on_message to a proper command

--- bot.py
+++ bot.py
@@ -1,27 +1,26 @@
 import requests
 import json
 import discord
-import asyncio
+from discord.ext import commands
 import random
 import os
 from datetime import datetime
 
-client = discord.Client()
+client = commands.Bot(command_prefix="++")
+
 
 
 @client.event
 async def on_ready():
-    await client.change_presence(activity=discord.Game(name="cmd: ++meneed gamename"))
+    print("Bot is has started running")
+    await client.change_presence(activity=discord.Game(name="cmd: ++search"))
 
-
-@client.event
-async def on_message(message):
-    if(message.author.id == your_bot_id_here):
-        return
-    search_term = message.content[9:len(message.content)].replace(" ", "%")
-    base_url = "https://www.googleapis.com/blogger/v3/blogs/your_blog_id/posts/search"
+@client.command()
+async def search(ctx, arg):
+    search_term = str(arg).replace(" ", "%")
+    base_url = "https://www.googleapis.com/blogger/v3/blogs/your_id/posts/search"
     complete_url = base_url + "?q=" + search_term + \
-        "&key=your_api_key_here"
+        "&key=your_token"
     response = requests.get(complete_url)
     result = response.json()
 
@@ -33,17 +32,12 @@ async def on_message(message):
                           description="Checked on " + f"{current_time}\n", color=0x349bfc)
     embed.set_author(name="Gaming Forecast")
     embed.set_thumbnail(url="https://www.gamingforecast.com/favicon.ico")
-
-    i = 0
-    while i < len(result["items"]):
-        title = result["items"][i]['title']
-        url = result["items"][i]['url']
-        embed.description = embed.description + f"{i+1}. [{title}]({url})\n"
-        i = i+1
+    for count, value in enumerate(result["items"]):
+        title = result["items"][count]["title"]
+        url = result["items"][count]["url"]
+        embed.description = embed.description + f"{count + 1}. [{title}]({url})\n"
     embed.set_footer(text='This message will be deleted in 1 Hour.')
 
-    if message.content.lower().startswith("++meneed"):
-        await message.channel.send(embed=embed, delete_after=3600.0)
+    await ctx.send(embed=embed, delete_after=3600.0)
 
-print("Bot is has started running")
-client.run(' bot token ')
+client.run("your_token")
