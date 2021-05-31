c9e662ec225a74f56f210a006dafbed085881e6f
DuhonTheGuy <duhontheguy@gmail.com>
2021-05-31 14:01:30 -0400

    Added a ping to the respective theme of the post every new post

--- bot.py
+++ bot.py
@@ -7,25 +7,29 @@ import random
 import os
 from datetime import datetime
 from googleapiclient.discovery import build
+import time
 
 client = commands.Bot(command_prefix="++")
 
-Key = "#"
-BlogID = "#"
+Key = "#"  # Replace with your API key
+BlogID = "#"  # Replace your BlogId here.
+
+Games = ['csgo', 'pubg', 'rust', 'valorant']
 
 blog = build("blogger", "v3", developerKey=Key)
 
+
 @client.event
 async def on_ready():
-    print("Bot is has started running")
+    print("Bot has started running")
     await client.change_presence(activity=discord.Game(name="cmd: ++search"))
 
 @client.command()
 async def search(ctx, arg):
     search_term = str(arg).replace(" ", "%")
-    base_url = "https://www.googleapis.com/blogger/v3/blogs/#/posts/search"
+    base_url = "https://www.googleapis.com/blogger/v3/blogs/" + BlogID + "/posts/search"
     complete_url = base_url + "?q=" + search_term + \
-        "&key=#"
+        "&key=" + Key
     response = requests.get(complete_url)
     result = response.json()
 
@@ -35,17 +39,24 @@ async def search(ctx, arg):
 
     embed = discord.Embed(title="List of Search results",
                           description="Checked on " + f"{current_time}\n", color=0x349bfc)
-    embed.set_author(name="Gaming Forecast")
+    embed.set_author(name="Your Website Here")
     embed.set_thumbnail(url="https://www.gamingforecast.com/favicon.ico")
-    for count, value in enumerate(result["items"]):
-        title = result["items"][count]["title"]
-        url = result["items"][count]["url"]
-        embed.description = embed.description + f"{count + 1}. [{title}]({url})\n"
-    embed.set_footer(text='This message will be deleted in 1 Hour.')
+    try:
+        for count, value in enumerate(result["items"]):
+            title = result["items"][count]["title"]
+            url = result["items"][count]["url"]
+            embed.description = embed.description + \
+                f"{count + 1}. [{title}]({url})\n"
+        embed.set_footer(text='This message will be deleted in 1 Hour.')
 
-    await ctx.send(embed=embed, delete_after=3600.0)
+        await ctx.send(embed=embed, delete_after=3600.0)
+    except:
+        await ctx.send("There is something wrong with the response.")
 
 client.recentPosts = None
+client.recentPostsTime = None
+client.recentPostsEdit = None
+
 
 @tasks.loop(seconds=5.0)
 async def fetchUpdates():
@@ -53,18 +64,23 @@ async def fetchUpdates():
     postsList = posts["items"]
     postTime = postsList[0]["published"]
     if not client.recentPosts:
-        client.recentPosts = postTime
-    elif client.recentPosts != postTime:
+        client.recentPostsTime = postTime
+        client.recentPosts = postsList
+    elif client.recentPostsTime != postTime:
         titleValue = str(posts["items"][0]["title"])
         urlValue = str(posts["items"][0]["url"])
-        channel = client.get_channel(#)
-        embed = discord.Embed(title="New posts to the blog!", description=f"[{titleValue}]({urlValue})")
-        embed.set_author(name="Gaming Forecast")
+        channel = client.get_channel(824401596695183400)  # Add channel ID
+        embed = discord.Embed(title="New posts to the blog!",
+                            description=f"[{titleValue}]({urlValue})")
+        embed.set_author(name="Your Website Name")
         embed.set_thumbnail(url="https://www.gamingforecast.com/favicon.ico")
+        for i in Games:
+            if i in postsList[0]["title"].lower():
+                await channel.send("@" + i)
         await channel.send(embed=embed)
-        client.recentPosts = postTime
+        client.recentPostsTime = postTime
+        client.recentPosts = postsList
 
 fetchUpdates.start()
 
-
-client.run("#")
+client.run("#")  # Your discord bot token here
